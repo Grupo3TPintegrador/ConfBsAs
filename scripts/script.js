617501e1a02fdee36d11832e471264d9414c7c14
Nicolas Barbetta <nbarbetta@gmail.com>
2023-10-20 17:35:58 -0300

    second commit validacion

--- scripts/script.js
+++ scripts/script.js
@@ -7,19 +7,46 @@ var descuento =[0.2, 0.5,0.85];
 function calcular(){
   let cant = parseInt(document.getElementById("cantidad").value);
   let cat = parseInt(document.getElementById("categoria").value);
-  if(Number.isInteger(cant) && cant !==0){
-    precioDescuento = precio * descuento[cat];
-    return cant * precioDescuento;
-  } else {
+  
+
+    if(Number.isInteger(cant) && cant > 0){
+      precioDescuento = precio * descuento[cat];
+      return cant * precioDescuento;
+    } else {
     alert("Debe ingresar una cantidad valida y distinta de 0")
     return 0
+    } 
+  
+}
+function validarTxt(){
+  let nombre = document.getElementById("txt_name").value;
+  let apellido = document.getElementById("txt_ape").value;
+  let mail = document.getElementById("txt_mail").value;
+  let  emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+  console.log(nombre+apellido+mail);
+  if(nombre ==="" || apellido ==="" || mail ===""){
+    alert("Debe completar todos los campos");
+    return false;
   }
+  if (emailRegex.test(mail)) {
+    console.log("Email OK")
+    return true;
+  } else {
+    alert("Email erroneo, corregir el campo")
+    return false;
+  }
+  
 }
 
-
-
 btnResumen.addEventListener("click", () => {
-  document.getElementById("resultado").innerText = calcular();
+  
+  if(validarTxt()){
+    document.getElementById("resultado").innerText = calcular();
+    console.log("verdadero");
+  }else{
+    console.log("false");
+    alert("Debe completar todos los campos")
+  }
 });
 
 btnBorrar.addEventListener("click", () => {
